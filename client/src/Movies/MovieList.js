d14ca602fabc1354549f602d998e2b1a59270af2
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-03-23 13:38:34 -0700

    finished with one of the stretch goals

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
 import { useParams, NavLink, useRouteMatch } from "react-router-dom";
 
 import axios from "axios";
@@ -25,33 +26,11 @@ const MovieList = props => {
     <div className="movie-list">
       {movies.map(movie => (
         <NavLink to={`/movies/${movie.id}`} key={movie.id}>
-          <MovieDetails movie={movie} />
+          <MovieCard movie={movie} />
         </NavLink>
       ))}
     </div>
   );
 };
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
-}
 
 export default MovieList;
